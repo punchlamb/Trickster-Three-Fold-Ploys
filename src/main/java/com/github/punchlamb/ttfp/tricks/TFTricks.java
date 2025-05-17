e4f73b6fcd8a57128b1c50d4c97e39e81a8f8e75
punchlamb <punchlamb@gmail.com>
2025-05-17 13:38:17 -0400

    testing

--- src/main/java/com/github/punchlamb/ttfp/tricks/TFTricks.java
+++ src/main/java/com/github/punchlamb/ttfp/tricks/TFTricks.java
@@ -1,23 +1,33 @@
 package com.github.punchlamb.ttfp.tricks;
 
+import com.github.punchlamb.ttfp.TricksterThreeFoldPloys;
+import com.github.punchlamb.ttfp.tricks.dimensionalism.BreakBlockBetterTrick;
+import com.mojang.serialization.Lifecycle;
+import dev.enjarai.trickster.spell.Pattern;
+import dev.enjarai.trickster.spell.trick.Trick;
+import dev.enjarai.trickster.spell.trick.Tricks;
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.SimpleRegistry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TFTricks {
 
+    //START OF TRICKS
 
+    public static final BreakBlockBetterTrick BREAK_BLOCK_BETTER =
+            register("break_block_better", new BreakBlockBetterTrick());
 
+    //END OF TRICKS
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private static <T extends Trick<?>> T register(String path, T trick) {
+        return Registry.register(Tricks.REGISTRY, TricksterThreeFoldPloys.id(path), trick);
+    }
 
     public static void register() {
         // I'm meowing SO HARD rn
---
+++ src/main/java/com/github/punchlamb/ttfp/tricks/dimensionalism/BreakBlockBetterTrick.java
@@ -0,0 +1,47 @@
+package com.github.punchlamb.ttfp.tricks.dimensionalism;
+
+import dev.enjarai.trickster.Trickster;
+import dev.enjarai.trickster.block.ModBlocks;
+import dev.enjarai.trickster.spell.EvaluationResult;
+import dev.enjarai.trickster.spell.Pattern;
+import dev.enjarai.trickster.spell.SpellContext;
+import dev.enjarai.trickster.spell.blunder.BlockTooHardBlunder;
+import dev.enjarai.trickster.spell.blunder.BlockUnoccupiedBlunder;
+import dev.enjarai.trickster.spell.blunder.BlunderException;
+import dev.enjarai.trickster.spell.fragment.FragmentType;
+import dev.enjarai.trickster.spell.fragment.VectorFragment;
+import dev.enjarai.trickster.spell.trick.Trick;
+import dev.enjarai.trickster.spell.type.Signature;
+
+import java.util.List;
+
+public class BreakBlockBetterTrick extends Trick<BreakBlockBetterTrick> {
+
+    public BreakBlockBetterTrick() {
+        super(Pattern.of(4, 5, 8, 4, 3, 0, 1, 4), Signature.of(FragmentType.VECTOR, BreakBlockBetterTrick::run));
+    }
+
+    public EvaluationResult run(SpellContext ctx, VectorFragment pos) throws BlunderException {
+        var blockPos = pos.toBlockPos();
+        var world = ctx.source().getWorld();
+
+        expectCanBuild(ctx, blockPos);
+
+        var state = world.getBlockState(blockPos);
+
+        if (state.isAir()) {
+            throw new BlockUnoccupiedBlunder(this, pos);
+        }
+
+        float hardness = state.getBlock().getHardness();
+
+        if (!state.isIn(ModBlocks.UNBREAKABLE) && hardness >= 0 && hardness < Trickster.CONFIG.maxBlockBreakingHardness()) {
+            ctx.useMana(this, Math.max(hardness, 8));
+            ctx.source().getCaster().ifPresentOrElse(c -> world.breakBlock(blockPos, true, c), () -> world.breakBlock(blockPos, true));
+        } else {
+            throw new BlockTooHardBlunder(this);
+        }
+
+        return pos;
+    }
+}
